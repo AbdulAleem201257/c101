fde1f83f1cb54377c60e186022bd8cdada7c6039
AbdulAleem201257 <145694229+AbdulAleem201257@users.noreply.github.com>
2023-12-21 20:38:18 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+//ADD YOUR FIREBASE LINKS
+
+
+
+
+
+function getData() {
+  firebase.database().ref("/").on('value', function (snapshot) {
+    document.getElementById("output").innerHTML = "";
+    snapshot.forEach(function (childSnapshot) {
+      childKey = childSnapshot.key;
+      Room_names = childKey;
+      //start code
+
+      //endcode
+    });
+  });
+
+}
+getData();
